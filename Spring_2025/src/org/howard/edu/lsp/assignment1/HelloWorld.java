323b010613bac5793debe7ecdfc52fa268526cb9
joshuaveasy <joshuaveasy@localhost>
2025-03-20 19:15:07 -0400

    added link to question 1

--- Spring_2025/src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ Spring_2025/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -1,11 +1,13 @@
 package org.howard.edu.lsp.assignment1;
 
+/**Joshua Veasy
+ * 
+ * @param args
+ */
+
 public class HelloWorld {
 
-	/**Joshua Veasy
-	 * 
-	 * @param args
-	 */
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
