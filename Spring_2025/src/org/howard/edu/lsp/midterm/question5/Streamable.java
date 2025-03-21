fa8525603fc938c338d55464681ad91938b883e2
Jmveasy14 <114871830+Jmveasy14@users.noreply.github.com>
2025-03-20 20:32:29 -0400

    Create Streamable.java

---
+++ Spring_2025/src/org/howard/edu/lsp/midterm/question5/Streamable.java
@@ -0,0 +1,7 @@
+package org.howard.edu.lsp.midterm.question5;
+
+public interface Streamable {
+    void play();   // Plays the content
+    void pause();  // Pauses the content
+    void stop();   // Stops the content
+}
